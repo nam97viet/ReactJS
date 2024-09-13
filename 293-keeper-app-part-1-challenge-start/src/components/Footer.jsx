381be016086fd87decb7325fc3db0045f5804b07
Nathan Tran <ntran1@cougarnet.uh.edu>
2024-09-13 18:41:33 -0500

    react props

---
+++ 293-keeper-app-part-1-challenge-start/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let d = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {d}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ 293-keeper-app-part-1-challenge-start/src/components/Heading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let name = "Keeper App";
+
+function Heading() {
+  return (
+    <header>
+      <h1>{name}</h1>
+    </header>
+  );
+}
+
+export default Heading;
